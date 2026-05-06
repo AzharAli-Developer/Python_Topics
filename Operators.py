f64aeea8ed9656e8f424857e69ca6a11aa542466
Azhar Ali <azharalisp059@gmail.com>
2026-05-06 10:16:27 +0500

    Operators

--- Operators.py
+++ Operators.py
@@ -1,3 +1,8 @@
-a = 5
-b = 6
-c = a + b
+"""In this section we will learn different concepts of python.
+like:
+    .Variables
+        .Assign multiple values to multiple variables.
+        .Assign one value to multiple variables.
+        .Local variables and global variables.
+        .Using the word of Global.
+"""
